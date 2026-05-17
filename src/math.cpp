7a39fb620f0c00fe882617481ed8b741582295c7
Kark Angelo <karkangelovaronapada@gmail.com>
2026-05-17 18:25:48 +0800

    fix: correct array length calculation in scaleDiff_CPU function

--- src/math.cpp
+++ src/math.cpp
@@ -157,7 +157,7 @@ Napi::Value scaleDiff_CPU(const Napi::CallbackInfo& info) {
     Napi::Float32Array arr1 = info[0].As<Napi::Float32Array>();
     Napi::Float32Array arr2 = info[1].As<Napi::Float32Array>();
     Napi::Float32Array arr3 = info[3].As<Napi::Float32Array>();
-    size_t arr_length = arr.ElementLength();
+    size_t arr_length = arr1.ElementLength();
     Napi::Float32Array output = Napi::Float32Array::New(env, arr_length);
 
     float* a1 = arr1.Data();
